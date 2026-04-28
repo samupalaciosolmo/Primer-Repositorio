df20f36f958ecebb3e356b06e09c320d4a42de32
samupalaciosolmo <samupalaciosolmo@gmail.com>
2026-04-28 19:18:00 +0200

    Update EjemploDocumento.java

--- EjemploDocumento.java
+++ EjemploDocumento.java
@@ -1,6 +1,7 @@
 public class EjemploDocumento {
     public static void main(String[] args) {
         System.out.println("Hola a mi mundo");
+        System.out.println("Me gustan las hamburguesas");
         public static  int multiplicacion(int a, int b) {
             return a * b;
         }
