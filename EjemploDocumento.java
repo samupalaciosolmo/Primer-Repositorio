1acfef6b6742ebda3f0c55d0b83df8e23a662502
samupalaciosolmo <samupalaciosolmo@gmail.com>
2026-04-28 18:39:42 +0200

    Añadir multiplicar

--- EjemploDocumento.java
+++ EjemploDocumento.java
@@ -1,5 +1,8 @@
 public class EjemploDocumento {
     public static void main(String[] args) {
         System.out.println("Hola a mi mundo");
+        public static int multiplicacion(int a, int b) {
+            return a * b;
+        }
     }
 }
